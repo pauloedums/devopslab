c3d3847a91f237e82a78cf899eaae57773e6ae75
Paulo Marques <pauloedums@gmail.com>
2021-10-09 12:07:44 -0300

    adicionando novo endpoint

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def pagina_inicial():
     return "Hello World"
 
+@app.route("/soma")
+def soma_valores():
+    x=10+10
+    return "Sua soma de 10+10="+str(x)
+
 if __name__ == '__main__':
     app.run()
