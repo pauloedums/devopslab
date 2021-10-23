6a8af93c6c5cd3605bd65a3084f210f0b26ade70
Vagrant User <vagrant@multicloud>
2021-10-23 10:36:47 -0300

    Retornando valores antigos

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)
-
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline Devops"
+    return "Hello World"
 
+@app.route("/soma")
+def soma_valores():
+    x=10+10
+    return "Sua soma de 10+10="+str(x)
 
 if __name__ == '__main__':
     app.run()
