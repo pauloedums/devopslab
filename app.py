def0932d3217b94b6a93dc769c8d388de7ff5a39
Vagrant User <vagrant@multicloud>
2021-10-23 10:10:33 -0300

    Bug Removido

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+    return "Laboratório Pipeline Devops"
 
 
 if __name__ == '__main__':
